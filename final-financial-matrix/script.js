996902af537c56cf5b8a467a2389efbb8532fa1a
thapa0301 <150643126+thapa0301@users.noreply.github.com>
2024-04-11 15:34:34 +0900

    Update script.js

--- final-financial-matrix/script.js
+++ final-financial-matrix/script.js
@@ -3,12 +3,24 @@ let screenTwo = document.querySelector("#screen2");
 
 let cButton = document.querySelectorAll(".c-button");
 
+let firstAxis = "";
+let secondAxis = "";
+
 cButton.forEach((btn) =>{
     btn.addEventListener("click", ()=>{
         let boxValue = btn.getAttribute("any");   //get desired clicked button value
 
         screenOne.style.display="none";
         screenTwo.style.display ="unset";
+
+        let lowerCaseBoxValue = boxValue.toLowerCase();
+
+        /* ------- second screen axis ------- */
+        firstAxis = "first-axis-".concat(lowerCaseBoxValue);
+        secondAxis = "second-axis-".concat(lowerCaseBoxValue);
+
+        let axisValue = ["", ""];
+        /* -------  end of second screen axis ------ */
 
         let arrValue = [];
         let bankName = [];
@@ -17,51 +29,58 @@ cButton.forEach((btn) =>{
             case "A":
                 bankName = ["●<span>横浜銀行</span><br>I:1.6％、L:90％", "●<span>三井トラスト銀行</span><br/>I:3.9％、L:100％", "●<span>静岡銀行</span><br/>I:3.3％、L:100％", ""];
                 arrValue = ["A1", "A2", "A3", "A4"];
+                axisValue = ["LTV(低)", "金利(高)"];
                 clickCenterBtn();
                 break;
 
             case "B":
                 bankName = ["", "", "", ""];
                 arrValue = ["B1", "B2", "B3", "B4"];
+                axisValue = ["金利(高)", "LTV(高)"];
                 clickCenterBtn();
                 break;
 
             case "C":
                 bankName = ["", "", "", ""];
-                arrValue = ["C1", "C2", "C3", "c4"];
+                arrValue = ["C1", "C2", "C3", "C4"];
+                axisValue = ["LTV(低)", "金利(低)"];
                 clickCenterBtn();
                 break;
 
             case "D":
                 bankName = ["", "", "", ""];
                 arrValue = ["D1", "D2", "D3", "D4"];
+                axisValue = ["金利(低)", "LTV(高)"];
                 clickCenterBtn();
                 break;
 
             case "E":
                 bankName = ["", "", "", ""];
                 arrValue = ["E1", "E2", "E3", "E4"];
+                axisValue = ["期間(短)", "金利(高)"];
                 clickCenterBtn();
                 break;
 
             case "F":
                 bankName = ["", "", "", ""];
                 arrValue = ["F1", "F2", "F3", "F4"];
+                axisValue = ["金利(高)", "期間(長)"];
                 clickCenterBtn();
                 break;
 
             case "G":
                 bankName = ["", "", "", ""];
                 arrValue = ["G1", "G2", "G3", "G4"];
+                axisValue = ["期間(短)", "金利(低)"];
                 clickCenterBtn();
                 break;
 
             case "H":
                 bankName = ["", "", "", ""];
                 arrValue = ["H1", "H2", "H3", "H4"];
+                axisValue = ["金利(低)", "期間(長)"];
                 clickCenterBtn();
                 break;
-
         }
 
         /* ----show the bankName in the screen 2---- */
@@ -76,8 +95,16 @@ cButton.forEach((btn) =>{
 
         /* -------Screen 2 center button------ */
         document.getElementById("center-button").innerHTML = boxValue;
-        
 
+        /* -------- add two axis class ------- */
+        document.querySelector("#first-axis").classList.add(firstAxis);
+        document.querySelector("#second-axis").classList.add(secondAxis);
+
+
+        for(let k = 0; k < axisValue.length; k++){
+            document.getElementsByClassName("screen2axis")[k].innerHTML = axisValue[k];
+        }
+        
     });
 });
 
@@ -87,5 +114,9 @@ const clickCenterBtn = () => {
     btn.addEventListener("click", () => {
         screenOne.style.display = "grid";
         screenTwo.style.display = "none";
-    })
+
+        /* --------- Screen 2 center button remove button ---------- */
+        document.querySelector("#first-axis").classList.remove(firstAxis);
+        document.querySelector("#second-axis").classList.remove(secondAxis);
+    });
 }
